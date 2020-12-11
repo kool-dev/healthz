a27a7223bde53c8736ed1210b0696584affcfc8a
Nikolas Abiusi <nikolasab@gmail.com>
2020-12-11 13:44:48 -0300

    refactor made to lean up root command file and export checks to a new file

---
+++ cmd/checks/checks.go
@@ -0,0 +1,82 @@
+package checks
+
+import (
+	"os/exec"
+	"net"
+	"net/http"
+	"github.com/anmitsu/go-shlex"
+	"fmt"
+	"encoding/json"
+
+)
+type toCheck struct {
+	Name      string `json:"name"`
+	Type      string `json:"type"`
+	Value	  string `json:"value"`
+}
+
+// InitChecks runs all the checks specified in the Json input string
+func InitChecks(input string) (err error) {
+	checks := make([]toCheck, 0)
+	json.Unmarshal([]byte(input), &checks)
+
+	for _, k := range checks {
+		switch k.Type {
+			case "tcp", "tcp4", "tcp6":
+				err = checkSocket(k.Type, k.Value)
+			case "http", "https":
+				err = checkHTTP(k.Value)
+			case "exec":
+				err = checkExec(k.Value)
+		}
+
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func checkSocket(socketType, url string) (err error) {
+	conn, err := net.Dial(socketType, url)
+
+	if conn != nil {
+		fmt.Printf("[TCP Check Success] Connected to %s://%s\n", socketType, url)
+	}
+
+	return
+}
+
+func checkHTTP(url string) (err error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("[HTTP Request Creation Error] Problem with dial: %v.\n", err.Error())
+	}
+
+	resp, err := client.Do(req)
+	if err == nil && resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		fmt.Printf("[HTTP Success] Received %d from %s\n", resp.StatusCode, url)
+	}
+
+	return
+}
+
+func checkExec(cmdValue string) (err error) {
+	args, err := shlex.Split(cmdValue, true)
+	if err != nil {
+		fmt.Printf("[Exec Parse Error] Exec args parse failed with %s\n", err)
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("[Exec Run Error] cmd.Run() failed with %s\n", err)
+		return
+	}
+
+	fmt.Println("[Exec Run Success] cmd.Run() ran successfully")
+	return
+}
--- cmd/root.go
+++ cmd/root.go
@@ -3,20 +3,10 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"encoding/json"
-	"net"
-	"net/http"
-
-	"github.com/anmitsu/go-shlex"
+	"./checks"
 	"github.com/spf13/cobra"
 )
 
-type toCheck struct {
-	Name      string `json:"name"`
-	Type      string `json:"type"`
-	Value	  string `json:"value"`
-}
 
 var (
 	jsonInput string
@@ -26,19 +16,15 @@ var (
 	Short: "Healthz checks if your application is healthy!",
 	Long: `A Fast simple and reliable tool to health check your application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		checks := make([]toCheck, 0)
-		json.Unmarshal([]byte(jsonInput), &checks)
+		err := checks.InitChecks(jsonInput)
 
-		for _, k := range checks {
-			switch k.Type {
-				case "tcp", "tcp4", "tcp6":
-					checkSocket(k.Type, k.Value)
-				case "http", "https":
-					checkHTTP(k.Value)
-				case "exec":
-					checkExec(k.Value)
-			}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
 		}
+
+		fmt.Println("\nAll checks ran successfully.\nApplication is healthy.")
+		os.Exit(0)
 	},
 }
 )
@@ -54,46 +40,4 @@ func Execute() {
 
 func init()  {
 	rootCmd.PersistentFlags().StringVarP(&jsonInput, "input", "i", "", "Json input to perform health checks")
-}
-
-func checkSocket(socketType, url string)  {
-	conn, err := net.Dial(socketType, url)
-	if err != nil {
-		fmt.Printf("Problem with dial: %v.\n", err.Error())
-	}
-	if conn != nil {
-		fmt.Printf("Connected to %s://%s\n", socketType, url)
-	}
-}
-
-func checkHTTP(url string) {
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Printf("Problem with dial: %v.\n", err.Error())
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Problem with request: %s.\n", err.Error())
-	} else if err == nil && resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		fmt.Printf("Received %d from %s\n", resp.StatusCode, url)
-	}
-}
-
-func checkExec(cmdValue string) {
-	args, err := shlex.Split(cmdValue, true)
-	if err != nil {
-		fmt.Printf("Exec args parse failed with %s\n", err)
-	}
-
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err = cmd.Run()
-	if err != nil {
-		fmt.Printf("cmd.Run() failed with %s\n", err)
-	}
 }
