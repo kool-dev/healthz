3170864d9042e60c70899c3b4d2886f3ff5d8fe9
Nikolas Abiusi <nikolasab@gmail.com>
2020-12-17 16:11:38 -0300

    first full test suite working and implemented

--- cmd/checks/checks_test.go
+++ cmd/checks/checks_test.go
@@ -3,33 +3,90 @@ package checks
 import (
 	"testing"
 	"net"
+	"net/http"
 	"errors"
 )
 
-func TestTCPErrorOutput(t *testing.T)  {
-	originalDialFn := dialFn
+func TestTCP(t *testing.T)  {
+	t.Run("Test Error Handling", func(t *testing.T) {
+		Checks.FuncDial = func(s, v string) (net.Conn, error) {
+			return nil, errors.New("connection error")
+		}
+		err := checkSocket("tcp", "localhost:80")
 
-	dialFn = func(s, v string) (net.Conn, error) {
-		return nil, errors.New("connection error")
-	}
-
-	defer func() {
-		dialFn = originalDialFn
-	}()
-
-	err := checkSocket("tcp", "localhost:80")
-
-	if err == nil {
-		t.Error("expecting error 'connection error', got none")
-	} else if err.Error() != "connection error" {
-		t.Errorf("expecting error 'connection error', got %v", err)
-	}
+		if err == nil {
+			t.Error("expecting error 'connection error', got none")
+		} else if err.Error() != "connection error" {
+			t.Errorf("expecting error 'connection error', got %v", err)
+		}
+	})
 }
 
 func TestHTTP(t *testing.T)  {
-	//@todo add logic here...
+	t.Run("Test Successful Response", func(t *testing.T) {
+		Checks.FuncDo = func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       nil,
+			}, nil
+		}
+
+		err := checkHTTP("http", "http://localhost:8080")
+
+		if err != nil {
+			t.Errorf("expecting no errors, got %s", err)
+		}
+	})
+
+	t.Run("Test 500 Server Response", func(t *testing.T) {
+		Checks.FuncDo = func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 500,
+				Body:       nil,
+			}, nil
+		}
+
+		err := checkHTTP("http", "http://localhost:8080")
+
+		if err == nil {
+			t.Errorf("expecting error for 500 response, got none")
+		}
+	})
+
+	t.Run("Test 500 Server Response", func(t *testing.T) {
+		Checks.FuncDo = func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+			}, errors.New("error on request")
+		}
+
+		err := checkHTTP("http", "http://localhost:8080")
+
+		if err == nil {
+			t.Error("expecting error 'error on request', got none")
+		} else if err.Error() != "error on request" {
+			t.Errorf("expecting error 'error on request', got %v", err)
+		}
+	})
 }
 
 func TestExec(t *testing.T)  {
-	//@todo add logic here...
+	t.Run("Valid Command Executes Successfully", func(t *testing.T) {
+		testCmd := "ls -lah"
+
+		err := checkExec("exec", testCmd)
+
+		if err != nil {
+			t.Errorf("expecting no errors, got %s", err)
+		}
+	})
+
+	t.Run("Invalid Command Returns Error", func(t *testing.T) {
+		testCmd := "invalid command"
+
+		err := checkExec("exec", testCmd)
+
+		if err == nil {
+			t.Error("expecting errors, got none")
+		}
+	})
 }
